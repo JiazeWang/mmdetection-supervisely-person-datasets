da038605643820b9f744b2673bd02d6b619e905d
JiazeWang <jiazewang@csu.edu.cn>
2020-02-12 22:00:42 +0800

    1

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -141,7 +141,7 @@ def show_result_new(img, result, dataset='coco', score_thr=0.3, out_file=None):
         for i in range(len(img)):
             if i not in inds:
                 color_mask = 0
-                mask = maskUtils.decode(segms[i]).astype(np.bool)
+                #mask = maskUtils.decode(segms[i]).astype(np.bool)
                 img[mask] = color_mask
     # draw bounding boxes
     #labels = [
