36fced6a934c938dd46727cd0bb0457d502bcf07
JiazeWang <jiazewang@csu.edu.cn>
2020-02-12 21:36:44 +0800

    init

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,6 +1,6 @@
 from .env import init_dist, get_root_logger, set_random_seed
 from .train import train_detector
-from .inference import inference_detector, show_result
+from .inference import inference_detector, show_result, show_result_new
 
 __all__ = [
     'init_dist', 'get_root_logger', 'set_random_seed', 'train_detector',
